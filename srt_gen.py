f54a7d715d819bbdf2b36d45657b2b8ffbcafbf7
liufangyuan09 <liufangyuan09@meituan.com>
2025-02-17 09:58:57 +0800

    完善工具

--- srt_gen.py
+++ srt_gen.py
@@ -50,48 +50,56 @@ def run_wisper(wav_file_path, srt_file_path, model_path, lang = 'zh'):
     ]
 
     print(f"try call {' '.join(cmds)}", flush=True)
-    ret = subprocess.call(cmds, stderr=subprocess.DEVNULL)
+    ret = subprocess.call(cmds, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     # ret = subprocess.call(cmds)
     if ret != 0:
         raise Exception("wisper failed")
 
-def convert_to_wav(audio_file_path, wav_file_path, start_time, end_time):
+def split_audio(audio_file_path, segment_len):
+    dir = os.path.dirname(audio_file_path)
+    base_name = os.path.basename(audio_file_path).split('.')[0]
+    project_folder = os.path.join(dir, base_name)
+
     cmds = [
         "ffmpeg",
         "-i", audio_file_path,
-        "-ss", str(start_time),
-        "-t", str(end_time - start_time),
+        "-f", "segment",
+        "-segment_time", str(segment_len),
+        "-c", "copy",
         "-ar", "16000",
         "-acodec", "pcm_s16le",
-        wav_file_path,
-        "-y"
+        os.path.join(project_folder, "%05d.wav"),
     ]
     print(f"try call {' '.join(cmds)}", flush=True)
+
+    time_point = datetime.now()
     ret = subprocess.call(cmds, stderr=subprocess.DEVNULL)
     if ret != 0:
-        raise Exception("convert_to_wav failed")
+        raise Exception("split audio failed")
+    time_consumed = datetime.now() - time_point
+    print(f"split audio {audio_file_path} cost {time_consumed}s")
 
 
-def gen_srt(audio_file, model = 'ggml-large-v2.bin', lang = 'zh', audio_seg_length = 1200, audio_offset = 0):
+
+def gen_srt(audio_file, model = 'ggml-large-v2.bin', lang = 'zh', audio_seg_length = 1200):
     dir = os.path.dirname(audio_file)
     base_name = os.path.basename(audio_file).split('.')[0]
     project_folder = os.path.join(dir, base_name)
     if not os.path.exists(project_folder):
         os.mkdir(project_folder)
-
-    # first get audio file length
-    audio_length = get_audio_length(audio_file)
+    split_audio(audio_file, audio_seg_length)
 
     model_path = f"models/{model}"
 
+    audio_files = glob.glob(f"{project_folder}/*.wav")
+    audio_files.sort()
+
     subs = []
-    for i in range(0, int((audio_length - audio_offset) / audio_seg_length) + 1):
+    for i in range(len(audio_files)):
         time_point = datetime.now()
-        start_time = i * audio_seg_length + audio_offset
-        end_time = min(start_time + audio_seg_length, audio_length)
-        wav_file = os.path.join(project_folder, f"{i:05d}.wav")
+        start_time = i * audio_seg_length
+        wav_file = audio_files[i]
         srt_file = os.path.join(project_folder, f"{i:05d}.srt")
-        convert_to_wav(audio_file, wav_file, start_time, end_time)
         run_wisper(wav_file, os.path.splitext(srt_file)[0], model_path, lang)
         sub = load_subs(srt_file)
         if not subs:
@@ -101,16 +109,21 @@ def gen_srt(audio_file, model = 'ggml-large-v2.bin', lang = 'zh', audio_seg_leng
             subs += offset_subs(sub, start_time, last_entry.index)
         save_srt(subs, os.path.join(dir, base_name + ".srt"))
         time_consumed = datetime.now() - time_point
-        print(f"audio length: {end_time - start_time}s, process time: {time_consumed.total_seconds()}s, process ratio: {(end_time - start_time) / time_consumed.total_seconds()}")
+        audio_length = get_audio_length(wav_file)
+        print(f"audio length: {audio_length}s, process time: {time_consumed.total_seconds()}s, process ratio: {audio_length / time_consumed.total_seconds()}")
 
     # shutil.rmtree(project_folder)
 
 if __name__ == "__main__":
-    # sub0 = load_subs('output.wav.srt')
-    # sub1 = load_subs('output.wav.srt')
-    # print(offset_subs(sub1, 300, sub0[-1].index))
+    files = [
+        # '镕亦看世界 - 苏东坡新传1.opus',
+        '镕亦看世界 - 苏东坡新传2.opus',
+        '镕亦看世界 - 苏东坡新传3.opus',
+        '镕亦看世界 - 全球通史1.opus',
+        '镕亦看世界 - 全球通史2.m4a',
+        '镕亦看世界 - 万历十五年.opus',
+    ]
 
-    # save_srt(sub0 + sub1, "output.wav1.srt")
-    # print(get_audio_length("./镕亦看世界 - 苏东坡新传1.opus"))
-
-    gen_srt(sys.argv[1], audio_offset=26 * 1200)
+    for f in files:
+        print(f"begin processing: {f}")
+        gen_srt(f)
